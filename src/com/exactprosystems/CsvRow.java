9263f80b7728bf33176fee5f0a399f2bfdd81f4b
maxim.kozyrev <blackforest95@mail.ru>
2018-04-27 12:07:42 +0300

    last commit

---
+++ src/com/exactprosystems/CsvRow.java
@@ -0,0 +1,241 @@
+package com.exactprosystems;
+
+import com.exactprosystems.dictionary.JavaType;
+
+public class CsvRow {
+
+    private String id;
+    private String message;
+    private String field;
+    private int lengthlse;
+    private int lengthbit;
+    private int lengthtq;
+    private int lengthob;
+    //private JavaType type;
+    private String type;
+    private String valuename;
+    private String description;
+    private String attribute;
+    private String exactprocomments;
+    private String specialremarkslse;
+    private String specialremarksbit;
+    private String specialremarkstq;
+    private String specialremarksob;
+    private String valuenamelse;
+    private String valuenamebit;
+    private String valuenametq;
+    private String valuenameols;
+    private String defaultvalue;
+    private int offsetlse;
+    private int offsetbit;
+    private int offsettq;
+    private int offsetob;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    /*public JavaType getType() {
+        return type;
+    }
+
+    public void setType(JavaType type) {
+        this.type = type;
+    }*/
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getValuename() {
+        return valuename;
+    }
+
+    public void setValuename(String valuename) {
+        this.valuename = valuename;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSpecialremarkslse() {
+        return specialremarkslse;
+    }
+
+    public void setSpecialremarkslse(String specialremarkslse) {
+        this.specialremarkslse = specialremarkslse;
+    }
+
+    public String getSpecialremarksbit() {
+        return specialremarksbit;
+    }
+
+    public void setSpecialremarksbit(String specialremarksbit) {
+        this.specialremarksbit = specialremarksbit;
+    }
+
+    public String getSpecialremarkstq() {
+        return specialremarkstq;
+    }
+
+    public void setSpecialremarkstq(String specialremarkstq) {
+        this.specialremarkstq = specialremarkstq;
+    }
+
+    public String getSpecialremarksob() {
+        return specialremarksob;
+    }
+
+    public void setSpecialremarksob(String specialremarksob) {
+        this.specialremarksob = specialremarksob;
+    }
+
+    public String getValuenamelse() {
+        return valuenamelse;
+    }
+
+    public void setValuenamelse(String valuenamelse) {
+        this.valuenamelse = valuenamelse;
+    }
+
+    public String getValuenamebit() {
+        return valuenamebit;
+    }
+
+    public void setValuenamebit(String valuenamebit) {
+        this.valuenamebit = valuenamebit;
+    }
+
+    public String getValuenametq() {
+        return valuenametq;
+    }
+
+    public void setValuenametq(String valuenametq) {
+        this.valuenametq = valuenametq;
+    }
+
+    public String getValuenameols() {
+        return valuenameols;
+    }
+
+    public void setValuenameols(String valuenameols) {
+        this.valuenameols = valuenameols;
+    }
+
+    public int getLengthlse() {
+        return lengthlse;
+    }
+
+    public void setLengthlse(int lengthlse) {
+        this.lengthlse = lengthlse;
+    }
+
+    public int getLengthbit() {
+        return lengthbit;
+    }
+
+    public void setLengthbit(int lengthbit) {
+        this.lengthbit = lengthbit;
+    }
+
+    public int getLengthtq() {
+        return lengthtq;
+    }
+
+    public void setLengthtq(int lengthtq) {
+        this.lengthtq = lengthtq;
+    }
+
+    public int getLengthob() {
+        return lengthob;
+    }
+
+    public void setLengthob(int lengthob) {
+        this.lengthob = lengthob;
+    }
+
+    public String getExactprocomments() {
+        return exactprocomments;
+    }
+
+    public void setExactprocomments(String exactprocomments) {
+        this.exactprocomments = exactprocomments;
+    }
+
+    public String getDefaultvalue() {
+        return defaultvalue;
+    }
+
+    public void setDefaultvalue(String defaultvalue) {
+        this.defaultvalue = defaultvalue;
+    }
+
+    public int getOffsetlse() {
+        return offsetlse;
+    }
+
+    public void setOffsetlse(int offsetlse) {
+        this.offsetlse = offsetlse;
+    }
+
+    public int getOffsetbit() {
+        return offsetbit;
+    }
+
+    public void setOffsetbit(int offsetbit) {
+        this.offsetbit = offsetbit;
+    }
+
+    public int getOffsettq() {
+        return offsettq;
+    }
+
+    public void setOffsettq(int offsettq) {
+        this.offsettq = offsettq;
+    }
+
+    public int getOffsetob() {
+        return offsetob;
+    }
+
+    public void setOffsetob(int offsetob) {
+        this.offsetob = offsetob;
+    }
+}
