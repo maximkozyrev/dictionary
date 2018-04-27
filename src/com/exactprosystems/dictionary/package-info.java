744bbf58766876e77ae64d2e9d0588e08c6df8d7
maxim.kozyrev <blackforest95@mail.ru>
2018-04-27 13:09:27 +0300

    last commit

---
+++ src/com/exactprosystems/dictionary/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://exactprosystems.com/dictionary", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.exactprosystems.dictionary;
